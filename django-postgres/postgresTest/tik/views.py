27ceee5fb45c7f5fe7af262d08bcc091bc91ce25
yoda-gell <hvuskv6@gmail.com>
2023-12-22 15:03:47 +0200

    try somsing

--- django-postgres/postgresTest/tik/views.py
+++ django-postgres/postgresTest/tik/views.py
@@ -1,19 +1,14 @@
-from rest_framework.parsers import FileUploadParser
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
-from .serializers import TikSerializer
+# views.py
+from django.shortcuts import render, redirect
+from .forms import FileUploadForm
 
+def upload_file(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('success')  # Redirect to a success page or another view
+    else:
+        form = FileUploadForm()
 
-class FileUploadView(APIView):
-    parser_class = (FileUploadParser,)
-
-    def post(self, request, *args, **kwargs):
-
-      file_serializer = TikSerializer(data=request.data)
-
-      if file_serializer.is_valid():
-          file_serializer.save()
-          return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-      else:
-          return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return render(request, 'upload.html', {'form': form})
