eb61cfc2e16eaf626dc6b588cdc03bf9b2b9a917
yoda-gell <hvuskv6@gmail.com>
2023-12-22 14:55:56 +0200

    .

--- django-postgres/postgresTest/tik/models.py
+++ django-postgres/postgresTest/tik/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Tik(models.Model):
     assign_to = models.ForeignKey('users.Client', related_name='tik', on_delete=models.CASCADE , default=None)
-    files = models.FileField(upload_to='files/', max_length=250,null=True,default=None)  
+    files = models.FileField(upload_to='files/', max_length=250)  
